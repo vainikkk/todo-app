78ecb019e3f4664926d5d07144b938142304f56f
vainik17 <vainik.kakadiya@infostretch.com>
2019-12-19 19:27:56 +0530

    css changes

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Todo = (props) => {
   return (
     <div>
-      <div className="list-group-item">
+      <div className="list-group-item overflow-hidden mb-1">
         {props.todo.name}
         <button className="btn-sm btn-danger float-right" onClick={props.deleteTodo}>X</button>
         <button className="btn-sm btn-info float-right mr-4" onClick={props.editTodo}>U</button>
